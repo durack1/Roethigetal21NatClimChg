7ee70c14f229afde79ef5164c4721ae3f0c4e761
Paul J. Durack <durack1@llnl.gov>
2021-07-26 13:06:28 -0700

    Correct grid asc lat ordering; update 1deg -> 025deg

--- readNcWriteAsc.py
+++ readNcWriteAsc.py
@@ -11,7 +11,9 @@ PJD 28 May 2021     - Consider stringIO module
                     Notes: https://www.geeksforgeeks.org/stringio-module-in-python/
                     https://stackoverflow.com/questions/24876331/writing-an-ascii-file-from-a-2d-numpy-array
 PJD 28 May 2021     - Updated to extract Palau data; Generated writeGridAscii function for generic outputs
-                    TODO: pull out Palau data
+PJD 26 Jul 2021     - Update Palau spatial range 2 to 10, 130 to 136 -> -10 to 20, 120 to 160
+PJD 26 Jul 2021     - flip upside down - https://github.com/durack1/Roethigetal21NatClimChg/issues/1
+                    TODO: ?
 
 Target:
 ncols        360
@@ -28,8 +30,10 @@ cellsize     1.000000000000
 import argparse
 import cdms2 as cdm
 import cdutil as cdu
+import datetime
 import numpy as np
 import pathlib
+#import pdb
 
 # %% function def
 
@@ -83,6 +87,14 @@ infile = str(args.infile)
 print('infile:', infile)
 outfile = str(args.infile).replace('.nc', '.txt')
 print('outfile:', outfile)
+# Append time to outfile, prevent overwrites
+timeNow = datetime.datetime.now();
+timeFormat = timeNow.strftime("%y%m%dT%H%M%S")
+# Find filename (exclude path and append timeFormat)
+outFileName = outfile.split('/')[-1]
+outFileNameNew = '_'.join([timeFormat, outFileName])
+outfile = outfile.replace(outFileName, outFileNameNew)
+print('outfile:', outfile)
 
 # %% read data
 fH = cdm.open(infile)
@@ -96,13 +108,35 @@ print('cellsize:', lat[1]-lat[0])
 #print('sos[0,:]:', sos[0,:])
 fH.close()
 
+# %% regrid data
+# Preload WOA18 grids
+#warnings.simplefilter('error')
+woa         = cdm.open('/work/durack1/Shared/obs_data/WOD18/190501_1210_WOD18_masks_0p25deg.nc')
+s           = woa('basinmask')
+woaGrid     = s.getGrid() ; # Get WOA target grid
+woaLat      = s.getLatitude()
+woaLon      = s.getLongitude()
+woa.close()
+sos025 = sos.regrid(woaGrid,regridTool='ESMF',regridMethod='linear')
+print('s:\n', sos.shape)
+print('s025:\n', sos025.shape)
+#pdb.set_trace()
+
 # %% write global data
 writeGridAscii(sos, outfile)
 
 # %% extract Palau data and write
-latBounds = [2, 10]
-lonBounds = [130, 136]
+latBounds = [-10, 20] ##[2, 10]
+lonBounds = [120, 160] ##[130, 136]
 palauDomain = cdu.region.domain(latitude=latBounds, longitude=lonBounds)
-sosPalau = palauDomain.select(sos)
-outfile = infile.replace('.nc', '_PalauDomain.txt')
+sosPalau = palauDomain.select(sos025)
+print('sosPalau:\n', sosPalau)
+# And flip upside down (N is on bottom, will be on top once flipped)
+sosPalau = np.flip(sosPalau, axis=0)
+print('sosPalau:\n', sosPalau)
+outfile = infile.replace('.nc', '_PalauDomain-025deg.txt')
+# Find filename (exclude path and append timeFormat)
+outFileName = outfile.split('/')[-1]
+outFileNameNew = '_'.join([timeFormat, outFileName])
+outfile = outfile.replace(outFileName, outFileNameNew)
 writeGridAscii(sosPalau, outfile)
