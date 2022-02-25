1c5894cb3a09699e4bbb4edddf9c81f1f65cdb3e
Paul J. Durack <durack1@llnl.gov>
2022-02-25 15:30:10 -0800

    update checkFiles.sh; syntax cleanup

--- checkFiles.sh
+++ checkFiles.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-# Ascertain files in a directory, print this and attempt to read
+# Ascertain netcdf/*.nc files in a directory, print header and attempt to
+# read variable
 
 # Paul J. Durack
 
 # PJD 25 Feb 2022 - Started for /p/css03/esgf_publish/CMIP6/ScenarioMIP/EC-Earth-Consortium/EC-Earth3/ssp370/r3i1p1f1/Omon/sos/gn/v20210517/
 # PJD 25 Feb 2022 - Added catch error after https://intoli.com/blog/exit-on-errors-in-bash-scripts/
+# PJD 25 Feb 2022 - Added help block
 
-
+## Add functional block
 # Add help block
 Help()
 {
    # Display Help
    echo "Provide netcdf file path and variable name and test"
-   echo "to read variable"
+   echo "to read file header and variable"
    echo
    echo "Syntax: checkFiles.sh [-h|v] filePath variableName"
    echo "options:"
@@ -37,17 +39,17 @@ set -e
 trap 'last_command=$current_command; current_command=$BASH_COMMAND; file=$filename' DEBUG
 # echo an error message before exiting
 trap 'echo "\"${last_command}\" command failed on ${file} with exit code $?."' EXIT
+## end functional block
 
-## Start functional script
-
-# get path, var arguments from command line and validate
+## Start operating script
+# get commandline filepath and var arguments and validate count
 echo "Total arguments: $#"
 if [[ "$#" != 2 ]]; then
     >&2 echo "$# is not enough arguments, quitting"
     exit 2
 fi
 
-# Assign commandline arguments to variables and start testing
+# Assign commandline arguments to variables and start file reads
 testPath=$1
 var=$2
 echo "Scanning for files in $testPath"
